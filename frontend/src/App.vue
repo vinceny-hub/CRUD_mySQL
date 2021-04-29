4612e9e58ee4f18073a60f412fb3f694c3e514e0
vinceny <vinceny@live.fr>
2021-04-29 19:48:02 +0300

    CRUD_mySQL

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,3 +23,6 @@ export default {
   name: "app"
 };
 </script>
+
+
+
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,26 +1,82 @@
-import Vue from "vue";
-import Router from "vue-router";
+// import Vue from "vue";
+// import Router from "vue-router";
+
+// Vue.use(Router);
+
+// export default new Router({
+//   mode: "history",
+//   routes: [
+//     {
+//       path: "/",
+//       alias: "/posts",
+//       name: "posts",
+//       component: () => import("./components/PostsList")
+//     },
+//     {
+//       path: "/posts/:id",
+//       name: "post-details",
+//       component: () => import("./components/Post")
+//     },
+//     {
+//       path: "/add",
+//       name: "add",
+//       component: () => import("./components/AddPost")
+//     }
+//   ]
+// });
+
+
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Register from './views/Register.vue';
 
 Vue.use(Router);
 
-export default new Router({
-  mode: "history",
+export const router = new Router({
+  mode: 'history',
   routes: [
     {
-      path: "/",
-      alias: "/posts",
-      name: "posts",
-      component: () => import("./components/PostsList")
+      path: '/',
+      name: 'home',
+      component: Home
     },
     {
-      path: "/posts/:id",
-      name: "post-details",
-      component: () => import("./components/Post")
+      path: '/home',
+      component: Home
     },
     {
-      path: "/add",
-      name: "add",
-      component: () => import("./components/AddPost")
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      // lazy-loaded
+      component: () => import('./views/Profile.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      // lazy-loaded
+      component: () => import('./views/BoardAdmin.vue')
+    },
+    {
+      path: '/mod',
+      name: 'moderator',
+      // lazy-loaded
+      component: () => import('./views/BoardModerator.vue')
+    },
+    {
+      path: '/user',
+      name: 'user',
+      // lazy-loaded
+      component: () => import('./views/BoardUser.vue')
     }
   ]
 });
